223eedfde39b55f70575228a5ebc5749bb1d847a
SagarSharma2004 <sagarsh1609@gmail.com>
2026-08-24 11:43:54 +0530

    new groq model

--- AIVA-Project/src/main/java/com/aiva/AIVA_Project/service/groqService.java
+++ AIVA-Project/src/main/java/com/aiva/AIVA_Project/service/groqService.java
@@ -18,7 +18,7 @@ public class groqService {
     @Value("${groq.api.key}")
     private String apiKey;
 
-    @Value("${groq.model:llama-3.1-8b-instant}")
+    @Value("${groq.model:GPT OSS 20B}")
     private String model;
 
     private final RestTemplate restTemplate = new RestTemplate();
